d95d1dd9ea09f650a90281a358d05534b3ab9e8e
kbar163 <kabarrantes163@gmail.com>
2022-09-02 14:33:19 -0600

    Separacion de controladores.
    Tras revisar las convenciones de creacion de codigo limpio para el framework, se separa el archivo en donde se encontraban los controladores. De ahora en adelante se creara un archivo por controlador.

--- Controllers/WorkerController.cs
+++ Controllers/WorkerController.cs
@@ -5,44 +5,6 @@ using TallerTECService.Models;
 namespace TallerTECService.Controllers
 {
 
-    //TallerController hereda la clase ControllerBase, utilizada para el manejo
-    //del endpoints.
-    //ApiController identifica a la clase como un controlador en el framework.
-    //TallerController se encarga de manejar el endpoint que permite a los usuarios hacer login.
-    //Route especifica la ruta para este controlador. En este caso local:
-    //http://localhost:5075/api/login
-    
-    [Route("api/login")]
-    [ApiController]
-    public class TallerController : ControllerBase
-    {
-        private readonly ITallerRepo _repository;
-
-        public TallerController(ITallerRepo repository)
-        {
-            _repository = repository;
-        }
-        
-        
-        // POST api/login
-        [HttpPost]
-        public ActionResult<AuthResponse> Authenticate(LoginData loginData)
-        {
-
-            var auth = _repository.authCheck(loginData);
-            if(auth.authenticated){
-                return Ok(auth);
-            }
-            else
-            {
-                return Unauthorized(auth);
-            }
-            
-            
-
-        }
-
-    }
 
     //WorkerController Se encarga de manejar operaciones CRUD para los trabajadores registrados.
     //Route especifica la ruta para este controlador. En este caso local:
